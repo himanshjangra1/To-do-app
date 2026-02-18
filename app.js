34eecdd8d5810ca3fc07ab4829b860855a33745f
himanshjangra1 <himanshujangra70152@gmail.com>
2026-02-18 14:24:29 +0530

    Added localStorage and Delete button feature.

--- app.js
+++ app.js
@@ -2,18 +2,27 @@ let newBtn = document.querySelector("#new-btn");
 let inputBox = document.querySelector("#input-box");
 let list = document.querySelector("#list");
 
-let tasks = JSON.parse(localStorage.getItem("tasks")) || []; // creating a STATE array. This loads saved tasks if they exists.
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 function saveTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
-} // this function saves the tasks to localstorage.
+}
 
 function renderTasks() {
     list.innerHTML = "";
 
     tasks.forEach((task, index) => {
         const li = document.createElement("li");
-        li.textContent = task.text;
+
+        const span = document.createElement("span");
+        span.textContent = task.text;
+
+        const delBtn = document.createElement("button");
+        delBtn.textContent = "❌";
+        delBtn.classList.add("delete-btn");
+
+        li.appendChild(span);
+        li.appendChild(delBtn);
 
         if (task.completed) {
             li.classList.add("completed");
@@ -24,16 +33,16 @@ function renderTasks() {
     });
 }
 
-
 newBtn.addEventListener("click", (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
+
     const value = inputBox.value.trim();
 
     if (value === "") {
         alert("Enter some task first!");
         return;
     }
-    
+
     tasks.push({
         text: value,
         completed: false
@@ -42,22 +51,37 @@ newBtn.addEventListener("click", (e) => {
     inputBox.value = "";
     saveTasks();
     renderTasks();
-})
+});
 
-list.addEventListener("dblclick", (e) => {
-     if (e.target.tagName === "LI") {
-        const index = e.target.dataset.index;
-        tasks[index].completed = !tasks[index].completed;
+list.addEventListener("click", (e) => {
+    const li = e.target.closest("li");
+    if (!li) return;
 
+    const index = li.dataset.index;
+
+    if (e.target.classList.contains("delete-btn")) {
+        tasks.splice(index, 1);
         saveTasks();
         renderTasks();
+        return;
     }
 });
 
+list.addEventListener("dblclick", (e) => {
+    const li = e.target.closest("li");
+    if (!li) return;
+
+    const index = li.dataset.index;
+    tasks[index].completed = !tasks[index].completed;
+
+    saveTasks();
+    renderTasks();
+});
+
 inputBox.addEventListener("keypress", (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
         newBtn.click();
     }
-})
+});
 
 renderTasks();
